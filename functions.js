3c629c78f62829e81bef8abb7e26848b57336724
Jose Janin <josemajanin@gmail.com>
2023-01-31 14:48:31 -0300

    Iniciando repositorio

---
+++ functions.js
@@ -0,0 +1,18 @@
+/* INFO PERSONAL */
+
+/* Funcion para mostrar info seleccionada*/
+
+function mostrarInfo(elementoMostrar) {    
+    document.getElementById(elementoMostrar).style.display = "block";   
+ }
+
+/*funcion que oculta seccion*/
+
+function ocultar(elementoOcultar){    
+    document.getElementById(elementoOcultar).style.display = "none";
+}
+
+
+
+
+
